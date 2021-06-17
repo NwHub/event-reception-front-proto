9a798383b20f8438010b34c004a3b0e81028de06
shirotori <shirotori83@gmail.com>
2021-06-17 13:14:42 +0900

    UPdate

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,133 +1,69 @@
 <template>
-  <v-container>
-    <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
-
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
-
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
-        </p>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ next.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-row>
-      </v-col>
-
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-row justify="center">
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-row>
+  <v-container fluid>
+    <v-row justify="center">
+      <v-subheader>Today</v-subheader>
+      <v-col v-for="event in eventList" :key="event.id" cols="12">
+        <v-card dark>
+          <v-card-title>{{ event.title }}</v-card-title>
+          <v-card-text style="white-space: pre-wrap; word-wrap: break-word">{{
+            event.body
+          }}</v-card-text>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn text color="secondary"> Cancel </v-btn>
+            <v-btn text color="primary" @click="writeToFirestore"> Save </v-btn>
+          </v-card-actions>
+        </v-card>
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+import { fireDb } from "@/plugins/firebase.js";
 export default {
   name: "HelloWorld",
-
   data: () => ({
-    ecosystem: [
-      {
-        text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader",
-      },
-      {
-        text: "github",
-        href: "https://github.com/vuetifyjs/vuetify",
-      },
-      {
-        text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify",
-      },
-    ],
-    importantLinks: [
-      {
-        text: "Documentation",
-        href: "https://vuetifyjs.com",
-      },
-      {
-        text: "Chat",
-        href: "https://community.vuetifyjs.com",
-      },
-      {
-        text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify",
-      },
-      {
-        text: "Twitter",
-        href: "https://twitter.com/vuetifyjs",
-      },
-      {
-        text: "Articles",
-        href: "https://medium.com/vuetify",
-      },
-    ],
-    whatsNext: [
-      {
-        text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer",
-      },
-      {
-        text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
-      },
-      {
-        text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
-      },
-    ],
+    eventList: [],
   }),
+  async created() {
+    const ref = fireDb.collection("dev").doc("event");
+    let snap;
+    try {
+      snap = await ref.get();
+    } catch (e) {
+      console.error(e);
+    }
+    this.eventList = snap.data().eventList;
+  },
+  methods: {
+    async writeToFirestore() {
+      const ref = fireDb.collection("dev").doc("event");
+
+      const document = {
+        eventList: [
+          {
+            id: 1,
+            title: "第二回モブプロ",
+            body: "新型コロナウイルス対策を厚生労働省に助言する専門家組織は16日、会合を開いた。\n新規感染者数は全国的に減少傾向にあるとしたうえで、東京都内では人の流れが増え続けているなどとし、感染再拡大への警戒心を強めている。",
+            dateTime: "2010-01-01 10:00",
+            href: "https://github.com/vuetifyjs/vuetify-loader",
+          },
+          {
+            id: 2,
+            title: "第３回モブプロ",
+            body: "新型コロナウイルス対策を厚生労働省に助言する専門家組織は16日、会合を開いた。新規感染者数は全国的に減少傾向にあるとしたうえで、東京都内では人の流れが増え続けているなどとし、感染再拡大への警戒心を強めている。",
+            dateTime: "2010-01-01 10:00",
+            href: "https://github.com/vuetifyjs/vuetify-loader",
+          },
+        ],
+      };
+      try {
+        await ref.set(document);
+      } catch (e) {
+        console.error(e);
+      }
+    },
+  },
 };
 </script>
